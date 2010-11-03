1eca62929e23bc2458516b90b61c432900e741d9
Nicos Gollan <gtdev@spearhead.de>
2010-11-03 17:14:49 +0100

    [merb-action-args] Advise about methopara on MRI 1.9.1

--- merb-action-args/lib/merb-action-args/get_args.rb
+++ merb-action-args/lib/merb-action-args/get_args.rb
@@ -4,9 +4,11 @@ else
   begin
     # 1.8: use ParseTree, which is not supported on 1.9 and Rubinius
     require File.dirname(__FILE__) / "mri_args"
-  rescue LoadError
+  rescue LoadError => e
     # use Method#parameters
-    raise unless Object.method(:inspect).respond_to? :parameters
+    unless Object.method(:inspect).respond_to? :parameters
+      raise LoadError.new("Make sure you require 'methopara' before merb-action-args if you want to use action args on Ruby 1.9.1: http://github.com/genki/methopara (original error: #{e.to_s})")
+    end
     require File.dirname(__FILE__) / "vm_args"
   end
 end
