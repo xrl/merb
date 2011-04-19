73da3c4d3865d66de7a4b52513a62e2573943780
Nicos Gollan <gtdev@spearhead.de>
2011-04-19 15:04:44 +0200

    [merb-exceptions] RSpec 2

--- merb-exceptions/spec/exceptions_helper_spec.rb
+++ merb-exceptions/spec/exceptions_helper_spec.rb
@@ -10,13 +10,15 @@ describe MerbExceptions::DefaultExceptionExtensions do
 
   it "should notify_of_exceptions" do
     MerbExceptions::Notification.should_receive(:new)
-    request("/not_found/index")
+    with_level(:fatal) do
+      visit("/not_found/index")
+    end
   end
 
   it "should log fatal on failure" do
     MerbExceptions::Notification.should_receive(:new).and_raise(StandardError)
     with_level(:fatal) do
-      request("/not_found/index")
+      visit("/not_found/index")
     end.should include_log("Exception Notification Failed:")
   end
 
