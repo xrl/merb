1418fa47a6a4d32d23645975bdaf46bc25f43fe9
Nicos Gollan <gtdev@spearhead.de>
2011-04-26 11:54:35 +0200

    Bump main gem versions to 1.2.0.pre1, update dependencies
    merb-gen now generates dependencies on:
    
      * DataMapper ~> 1.1
      * DataObjects ~> 0.10

--- merb-helpers/lib/merb-helpers/version.rb
+++ merb-helpers/lib/merb-helpers/version.rb
@@ -1,5 +1,5 @@
 module Merb
   module Helpers
-    VERSION = '1.2.0'.freeze
+    VERSION = '1.2.0.pre1'.freeze
   end
 end
--- merb-slices/lib/merb-slices/version.rb
+++ merb-slices/lib/merb-slices/version.rb
@@ -1,5 +1,5 @@
 module Merb
   module Slices
-    VERSION = '1.2.0'.freeze
+    VERSION = '1.2.0.pre1'.freeze
   end
 end
